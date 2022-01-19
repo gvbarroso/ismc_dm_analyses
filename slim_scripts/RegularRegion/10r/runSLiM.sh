7b88a6827c03b3c5122c14bb2fd573ca515d1bd0
Julien Y.  Dutheil <dutheil@evolbio.mpg.de>
2022-01-19 15:46:24 +0100

    Added SLIM files.

---
+++ slim_scripts/RegularRegion/10r/runSLiM.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#SBATCH --job-name=bgs-slim-30mb-10r
+#SBATCH --ntasks=1 # num of cores
+#SBATCH --nodes=1
+#SBATCH --time=300:00:00 # in hours
+#SBATCH --mem=100G 
+#SBATCH --error=log/bgs_slim_30mb.%J.err
+#SBATCH --output=log/bgs_slim_30mb.%J.out
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=####
+#SBATCH --partition=global 
+#SBATCH --array=1-10
+
+~/.local/bin/slim -d seed=$((SLURM_ARRAY_TASK_ID + 42)) -d rep=$SLURM_ARRAY_TASK_ID Simulate.slim
+  
